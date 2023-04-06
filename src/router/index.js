ef6a924803a700674bb3c76e99db5c99e51a1355
trieuquocdatb1910052 <datb1910052@student.ctu.edu.vn>
2023-04-06 21:25:41 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,12 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
